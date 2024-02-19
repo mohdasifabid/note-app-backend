9813068b55f0440f25f431f582a6dedd1a87db8e
Mohd Asif <masifabid@outlook.com>
2024-02-19 23:44:35 +0530

    Add all CRUD operations for RESTful

--- index.js
+++ index.js
@@ -1,36 +1,16 @@
-const mongoose = require("mongoose");
-
-mongoose
-  .connect("mongodb://localhost/notes-app")
-  .then(() => console.log("Connected successfuly to the DB"))
-  .catch(() => console.log("Error : Connection Failed"));
-
-const notesSchema = mongoose.Schema({
-  id: Number,
-  title: String,
-  content: String,
-  label: String,
+const express = require("express");
+const notes = require("./routes/notes");
+const { connect, default: mongoose } = require("mongoose");
+connect("mongodb://localhost:27017/notes-app").then(() => {
+  console.log("connected to db");
 });
+const app = express();
+app.use(express.json());
+const port = process.env.PORT || 9000;
 
-const Note = mongoose.model("Note", notesSchema);
-async function createNote() {
-  const notes = new Note({
-    title: "Build Note App",
-    content: "Do proper testing",
-    label: "New Project",
-  });
-  const result = await notes.save();
-  console.log(result);
-}
-createNote();
-async function getNotesList() {
-  const pageNumber = 2;
-  const pageSize = 10;
-  const notes = await Note.find()
-    .skip((pageNumber - 1) * pageSize)
-    .limit(pageSize)
-    .sort("title")
-    .select(" _id title content label");
-  console.log({ notes });
-}
-getNotesList();
+app.use("/api/notes", notes);
+app.listen(port, () => console.log(`Listening on Port ${port}`));
+
+//port
+//connection to db
+//apis and all
